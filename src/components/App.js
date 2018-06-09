a0ef37a5a29e1e15c02f82d0391af484fedcf4de
Edi Rose <edirose1998@gmail.com>
2018-06-10 11:04:25 +1200

    added stage in state

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,20 @@ import React, { Component } from 'react';
 import '../App.css';
 
 class App extends Component {
+  constructor (props) {
+  super(props)
+  this.state = {
+    stage: 0
+  }
+  this.nextStage = this.nextStage.bind(this)
+}
+  nextStage() {
+  this.setState({stage: this.state.stage++})
+  }
+
+  componentWillMount(){
+    console.log(this.state.stage)
+  }
   render() {
     return (
         <p>Hello world!</p>
