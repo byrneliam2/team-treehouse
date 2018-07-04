d52dddabfa00be8aeffc9c354667e766c67b2a13
Liam <liam.jb1@gmail.com>
2018-07-04 21:38:31 +1200

    2 spaces

--- src/Services.js
+++ src/Services.js
@@ -1,43 +1,43 @@
 var services = [
-    {
-      name: 'immigration',
-      persons: ['immigrant', 'sdf'],
-   
-    },
-    {
-      name: 'transport',
-      persons: ['elderly', 'student'],
-    },
-    {
-      name: 'government',
-      persons: ['resident', 'elderly'],
-    },
-    {
-      name: 'health',
-      persons: ['elderly'],
-    },
-    {
-      name: 'benefits',
-      persons: ['elderly', 'disabled'],
-      url: 'health'
-    },
-    {
-      name: 'work',
-      persons: ['resident', 'married'],
-      url: 'work url'
+  {
+    name: 'immigration',
+    persons: ['immigrant', 'sdf'],
+
+  },
+  {
+    name: 'transport',
+    persons: ['elderly', 'student'],
+  },
+  {
+    name: 'government',
+    persons: ['resident', 'elderly'],
+  },
+  {
+    name: 'health',
+    persons: ['elderly'],
+  },
+  {
+    name: 'benefits',
+    persons: ['elderly', 'disabled'],
+    url: 'health'
+  },
+  {
+    name: 'work',
+    persons: ['resident', 'married'],
+    url: 'work url'
+  }
+]
+
+function getServices(person) {
+  var found = []
+  for (var i = 0; i < services.length; i++) {
+    if (person.some(r => services[i].persons.includes(r))) {
+      found.push(services[i].name)
     }
-   ]
-   
-   function getServices(person) {
-    var found = []
-    for (var i = 0; i < services.length; i++) {
-      if (person.some(r => services[i].persons.includes(r))) {
-        found.push(services[i].name)
-      }
-    }
-    return found
-   }
-   
-   var personConstraints = ['elderly', 'parent', 'resident', 'disabled', 'unemployed'];
-   
-   console.log(getServices(personConstraints));
+  }
+  return found
+}
+
+var personConstraints = ['elderly', 'parent', 'resident', 'disabled', 'unemployed'];
+
+console.log(getServices(personConstraints));
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,35 +4,35 @@ import './App.css'
 
 class App extends Component {
 
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = {
-        step: 0
+      step: 0
     }
     this.nextStep = this.nextStep.bind(this)
 
     document.body.style = 'background: #0f0f0f;';
   }
-  
+
   nextStep() {
-    this.setState({step: this.state.step + 1 })
+    this.setState({ step: this.state.step + 1 })
     console.log(this.state.step)
   }
 
-  componentWillMount(){
-  
+  componentWillMount() {
+
   }
-  
+
   render() {
     return (
       <main>
         <div>
-            <img class="govtimg" src="/govt_inv.png" alt="New Zealand Government"/>
+          <img class="govtimg" src="/govt_inv.png" alt="New Zealand Government" />
         </div>
         <div className="progressBar">
         </div>
         <div>
-          <Landing prevStep={this.prevStep} nextStep={this.nextStep} step={this.state.step}/>
+          <Landing prevStep={this.prevStep} nextStep={this.nextStep} step={this.state.step} />
         </div>
       </main>
     );
